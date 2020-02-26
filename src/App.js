c096d3bb9c3a052b034fa93834fb7e04a5bebfe2
Rex Bergeron <rexbergeron@comcast.net>
2020-02-26 15:52:10 -0800

    working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import TodoApp from './TodoApp.js';
 import './App.css';
+import Header from './Header.js'
+import { 
+    BrowserRouter as Router, 
+    Route, 
+    Switch,
+} from 'react-router-dom';
+ 
+class App extends Component {
+    render() {
+    return (
+        <div className="App">
+            <Header />
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={ TodoApp }/>
+                </Switch>
+            </Router>
+
+        </div>
+    );
+}
 }
 
 export default App;
